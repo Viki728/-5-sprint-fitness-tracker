ad107ac0e35aab597b2b8f74d094bb4492814b3a
Viktoriy Malukova <malukova-99@ya.ru>
2025-07-24 22:07:51 +0300

    first commit

--- internal/personaldata/personaldata.go
+++ internal/personaldata/personaldata.go
@@ -1,9 +1,17 @@
 package personaldata
 
+import "fmt"
+
 type Personal struct {
-	// TODO: добавить поля
+	//Создаем поля структуры
+	Name   string
+	Weight float64
+	Height float64
 }
 
 func (p Personal) Print() {
-	// TODO: реализовать функцию
+	//Выводим данные структуры на экран
+	fmt.Println("Имя: ", p.Name)
+	fmt.Println("Вес: ", p.Weight)
+	fmt.Println("Рост: ", p.Height)
 }
